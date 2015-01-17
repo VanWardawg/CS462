c849e44b0a2f1c7c09b9df9b1a9682d0260ac145
Kevin Hinton <kevinhinton@outlook.com>
2015-01-16 18:39:15 -0700

    modified

--- Lab1/cgi-bin/test.pl
+++ Lab1/cgi-bin/test.pl
@@ -1,12 +1,3 @@
-###Start###
-
-#!/usr/bin/perl -w
-print "Content-type: text/html\r\n\r\n";
-print "Hello there!<br />\nJust testing .<br />\n";
-
-for ($i=0; $i<10; $i++)
-{
-print $i."<br />";
-}
-
-###End###
+#!/usr/bin/perl
+print "Content-type: text/html\n\n";
+print "Hello, World.";
