5c4f772b079ab4539891c3dd1697853f6504a717
Kevin Hinton <wardawg6612@gmail.com>
2015-02-04 17:03:09 -0700

    add users file

--- backend/app/app.js
+++ backend/app/app.js
@@ -8,13 +8,15 @@ app.use(express.bodyParser());
 
 app.listen(3000);
 
+var data = require('/home/ubuntu/dev/CS462/backend/app/data.json');
+
 app.get('/backend', function(req, res) {
     res.send('Bonjour tout le monde!');
 });
 
 app.get('/backend/users', function (req, res) {
     try{
-      var data=fs.readFileSync('/home/ubuntu/dev/CS462/backend/app/users.txt','utf8');
+      var data2=fs.readFileSync('/home/ubuntu/dev/CS462/backend/app/users.txt','utf8');
       res.end(data);
     }catch(e){
       res.end("No such file or directory");
