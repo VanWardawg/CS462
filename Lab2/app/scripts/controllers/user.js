b548428c568ff0b4c86f429d09beb43ff95df285
Kevin Hinton <wardawg6612@gmail.com>
2015-02-05 17:11:12 -0700

    small change

--- Lab2/app/scripts/controllers/user.js
+++ Lab2/app/scripts/controllers/user.js
@@ -32,11 +32,20 @@ angular.module('Lab2App').controller('UserCtrl', ['$scope','$routeParams','$root
 		$scope.setUser();
 	}
 
+	$scope.connectToApp = function() {
+		$http.get('https://api.foursquare.com/v2/users/self/').success(function(data){
+			$scope.user.id = data.response.user.id;
+			console.log(data);
+			$http.put('https://52.0.11.73/backend/users',$scope.user).success(function() {
 
+			});
+		});
+	}
 
 	$scope.getMyCheckins = function() {
-		$http.get('https://api.foursquare.com/v2/checkins/recent').success(function(data){
-			$scope.user.checkins = data.response.recent;
+		$http.get('https://api.foursquare.com/v2/users/self/checkins').success(function(data){
+			$scope.user.checkins = data.response.checkins.items;
+			console.log(data);
 			$http.put('https://52.0.11.73/backend/users',$scope.user).success(function() {
 
 			});
